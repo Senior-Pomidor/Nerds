7c5a7335a26df71c93ba71825bcd5ecece521030
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-04-17 19:40:48 +0300

    added script for slider

--- slider.js
+++ slider.js
@@ -8,9 +8,31 @@ slides[activeIndex].classList.add(activeClass);
 
 for(let i = 0; i < slides.length; i++) {
    //навешивает обработчик на каждую радиокнопку
-   sliderControls[i].addEventListener('change', function() {
-      slides[i].classList.add(activeClass);
-      slides[activeIndex].classList.remove(activeClass);
-      activeIndex = i;
+   sliderControls[i].addEventListener('click', function(evt) {
+      evt.preventDefault()
+      // slides[i].classList.add(activeClass);
+      // slides[activeIndex].classList.remove(activeClass);
+      // activeIndex = i;
    });
-};
+}
+
+let next = document.querySelector('.next')
+
+function nextSlide() {
+   slides[activeIndex].classList.remove(activeClass);
+   sliderControls[activeIndex].removeAttribute('checked', '');
+
+   if(activeIndex >= slides.length - 1) {
+      activeIndex = -1
+   }
+   activeIndex++
+
+   slides[activeIndex].classList.add(activeClass);
+
+   sliderControls[activeIndex].setAttribute('checked', '');
+   console.log(activeIndex)
+}
+
+setInterval(function() {
+  document.querySelector('.next').click();
+}, 4000)
